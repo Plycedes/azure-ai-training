e00e279a7cc5bff67482b00be3c413331618a7e9
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-24 22:27:10 +0530

    Update vite.config.js

--- image-classifier/frontend/vite.config.js
+++ image-classifier/frontend/vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        host: "0.0.0.0", // Listen on all network interfaces
+        port: 5173, // Ensure the port matches your setup
+        proxy: {
+            "/analyze-image": {
+                target: "http://20.127.172.135:5000", // Replace with your Flask backend address
+                changeOrigin: true, // Change the origin of the host header to the target URL
+            },
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*", // This allows all origins, adjust as needed
+        },
+    },
 });
-
