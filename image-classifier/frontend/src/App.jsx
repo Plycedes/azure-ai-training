f04896880a2e47110321fefdfd731bcffd510ab4
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-27 10:20:16 +0530

    Update App.jsx

--- image-classifier/frontend/src/App.jsx
+++ image-classifier/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"; // Ensure you import Axios
+import axios from "axios";
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null);
